7867194ffacd83b27721f49fa11355e71831a148
Bekmyrza <bekmyrzabekkariev05@gmail.com>
2023-12-16 19:55:11 +0600

    ss

---
+++ src/qa_guru_17_java_Basic/src/Main.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+	public static void main(String[] args) {
+		// Типы данных - Бесконечность
+		// Примитивные
+		// 8 Главных типов данных
+
+		// логические
+		boolean varBoolean = true; // false
+		// целочисленные
+		byte varByte; // 8 bit
+		short varShort; // 16 bit
+		int varInt; // 32 bit
+		long varLong; // 64 bit
+
+
+
+
+	}
+}
