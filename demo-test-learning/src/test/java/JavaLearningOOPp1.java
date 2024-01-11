d48f3da14b2a839f13010970ddbac43e2d892fe5
bekmyrza <bekmyrzabekkariev05@gmail.com>
2024-01-11 19:57:04 +0600

    test

---
+++ demo-test-learning/src/test/java/JavaLearningOOPp1.java
@@ -0,0 +1,5 @@
+public class JavaLearningOOPp1 {
+	public static void main(String[] args) {
+		String s1;
+	}
+}
--- demo-test-learning/src/test/java/firsTest.java
+++ demo-test-learning/src/test/java/firsTest.java
@@ -1,10 +1,10 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 public class firsTest {
 	@Test
 	void testdocker() {
+		Configuration.holdBrowserOpen = true;
 //		Configuration.holdBrowserOpen = true;
 		open("https://google.com");
 		$("#W0wltc").click();
